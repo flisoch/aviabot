206f8a635d95efb04d0678195fbe80fb15f17f78
flisoch <flisochcs@gmail.com>
2019-01-17 20:01:20 +0300

    add webhook

--- bot.py
+++ bot.py
@@ -2,12 +2,56 @@
 import datetime
 import time
 
+import logging
+import ssl
+
+from aiohttp import web
+
 import telebot
 from telebot import types
 import secret
 from Calculator import *
 
-bot = telebot.TeleBot(secret.TOKEN, threaded=False)
+API_TOKEN = secret.TOKEN
+
+WEBHOOK_HOST = 'fierce-island-30585.heroku.com'
+# WEBHOOK_HOST = '176.52.6.160'
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(API_TOKEN)
+
+app = web.Application()
+
+# Process webhook calls
+async def handle(request):
+    if request.match_info.get('token') == bot.token:
+        request_body_dict = await request.json()
+        update = telebot.types.Update.de_json(request_body_dict)
+        bot.process_new_updates([update])
+        return web.Response()
+    else:
+        return web.Response(status=403)
+
+
+app.router.add_post('/{token}/', handle)
 
 
 @bot.message_handler(commands=['start'])
@@ -164,9 +208,22 @@ def ignore(call):
     bot.answer_callback_query(call.id, text="")
 
 
-while True:
-    try:
-        bot.polling(none_stop=True, timeout=31)
-    except Exception as e:
-        telebot.logger.error(e)
-        time.sleep(15)
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+# bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+
+# Build ssl context
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+
+# Start aiohttp server
+web.run_app(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+    ssl_context=context,
+)
+
